1cfadb3a5972f20af352b403a44aa19e1b84459e
Ben <bmsteve96@gmail.com>
2022-05-22 23:20:42 +0300

    test creating api request

--- src/main.ts
+++ src/main.ts
@@ -6,17 +6,38 @@ const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN')
 const BRANCH_NAME = core.getInput('BRANCH_NAME')
 const FETCH_ON_MERGE_PR = core.getInput('FETCH_ON_MERGE_PR')
 const JIRA_ISSUE_API_URL = core.getInput('JIRA_ISSUE_API_URL')
+const JIRA_AUTH_TOKEN = core.getInput('JIRA_AUTH_TOKEN')
 const octokit = github.getOctokit(GITHUB_TOKEN)
 const {context = {}}: any = github
 
 async function run() {
   try {
     core.info('Hell world')
-    // fetch branch name if not provided
-    // fetch jira issue
+    core.info(JIRA_ISSUE_API_URL)
+    if (!BRANCH_NAME) {
+      core.info(BRANCH_NAME)
+      fetch_issue('TO-169')
+    } else if (!BRANCH_NAME && FETCH_ON_MERGE_PR) {
+    } else {
+    }
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
 }
 
 run()
+
+const fetch_issue = (issue: any) => {
+  axios
+    .get(`${JIRA_ISSUE_API_URL}/${issue}`, {
+      headers: {
+        Authorization: `Basic ${JIRA_AUTH_TOKEN}`
+      }
+    })
+    .then((res: any) => {
+      core.info(res)
+    })
+    .catch((err: any) => {
+      core.info(err.message)
+    })
+}
